e5a73d685cd599d3941d80d22bc78b1a8ac0cfa0
jaanus <jaanus@gmail.com>
2015-05-04 21:48:32 +0200

    Initial Commit

---
+++ Typesaver/TypesaverView.h
@@ -0,0 +1,13 @@
+//
+//  TypesaverView.h
+//  Typesaver
+//
+//  Created by Jaanus Kase on 04/05/15.
+//  Copyright (c) 2015 Typesaver. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface TypesaverView : ScreenSaverView
+
+@end
