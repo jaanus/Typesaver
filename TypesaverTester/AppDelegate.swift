9b8a98de72cfe592e445a979a1bd03024f0784c4
jaanus <jaanus@gmail.com>
2015-05-05 12:14:03 +0200

    Correct tester window reopen/redisplay behavior

--- TypesaverTester/AppDelegate.swift
+++ TypesaverTester/AppDelegate.swift
@@ -11,14 +11,19 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
+    }
+    
+    func applicationShouldHandleReopen(sender: NSApplication, hasVisibleWindows: Bool) -> Bool {
+        for window in sender.windows as! [NSWindow] {
+            window.makeKeyAndOrderFront(self)
+        }
+        return false
     }
 
 
